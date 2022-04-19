0f056195a572a076ece093f168b8eb8696b2bcfa
fremix <microsys.net@gmail.com>
2022-04-19 13:10:39 -0500

    Selectores del DOM

---
+++ js/22.js
@@ -0,0 +1,25 @@
+/** Selectores del DOM 
+ * Para seleccionar una clase se coloca .nombredelaClase
+ * Para seleccionar un ID se usa el numeral #nombredelID
+ * Para seleccionar un elemento HTML, solo se le pasa en elemento HTML
+*/
+
+const heading = document.querySelector('.heading');
+
+/*
+console.log(heading);
+console.log(heading.textContent);
+console.log(heading.tagName);
+console.log(heading.classList);
+console.log(heading.id);
+*/
+
+const enlaces = document.querySelectorAll('.navegacion a');
+
+console.log(enlaces);
+
+
+
+
+
+
